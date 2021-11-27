fec542a79e1ca7095a1fde4b3b28b0c02e9b5785
Alexandra Ilyashenko <sasha.ilyashenko@onde.app>
2021-11-27 21:29:39 +0300

    feat: add app.js with server & person controller with all routes

---
+++ app.js
@@ -0,0 +1,36 @@
+const http = require("http")
+const router = require("./src/router")
+
+require("./src/Person")
+
+const requestListener = function (request, response) {
+  const currentRoute = router.getRoute(request.url, request.method)
+
+  if (!currentRoute) {
+    response.writeHead(404)
+    response.end(JSON.stringify({ message: "Route not found" }))
+
+    return
+  }
+
+  let body = ""
+
+  request.on("data", (chunk) => {
+    body += chunk
+  })
+
+  request.on("end", () => {
+    try {
+      request.body = body
+      request.params = currentRoute.params
+
+      currentRoute.callback(request, response)
+    } catch (err) {
+      response.writeHead(500)
+      response.end(JSON.stringify({ message: "Something went wrong!" }))
+    }
+  })
+}
+
+const server = http.createServer(requestListener)
+server.listen(9900)
---
+++ src/Person/controller.js
@@ -0,0 +1,116 @@
+const uuid = require("uuid")
+const router = require("../router/index")
+const personModel = require("./model")
+const personValidator = require("./validator")
+
+const _validateId = (value) => {
+  return uuid.validate(value) && uuid.version(value) === 4
+}
+
+// Get persons
+router.get("/person", (request, response) => {
+  const result = personModel.getAll()
+
+  response.writeHead(200, { "Content-Type": "application/json" })
+  response.end(JSON.stringify(result))
+})
+
+// Get person by personId
+router.get("/person/${personId}", (request, response) => {
+  const { personId } = request.params || {}
+
+  if (!_validateId(personId)) {
+    response.writeHead(400)
+    response.end(JSON.stringify({ message: "Parameter 'personId' is not valid" }))
+
+    return
+  }
+
+  const result = personModel.getById(personId)
+
+  if (!result) {
+    response.writeHead(404)
+    response.end(JSON.stringify({ message: "Person not found" }))
+
+    return
+  }
+
+  response.writeHead(200, { "Content-Type": "application/json" })
+  response.end(JSON.stringify(result))
+})
+
+// Post person
+router.post("/person", (request, response) => {
+  const data = JSON.parse(request.body) || {}
+  const errors = personValidator.validate(data)
+
+  if (errors.length) {
+    response.writeHead(400)
+    response.end(JSON.stringify({ message: errors.join(" ") }))
+
+    return
+  }
+
+  const result = personModel.create(data)
+
+  response.writeHead(201, { "Content-Type": "application/json" })
+  response.end(JSON.stringify(result))
+})
+
+// Put person
+router.put("/person/${personId}", (request, response) => {
+  const { personId } = request.params || {}
+  const data = JSON.parse(request.body) || {}
+
+  if (!_validateId(personId)) {
+    response.writeHead(400)
+    response.end(JSON.stringify({ message: "Parameter 'personId' is not valid" }))
+
+    return
+  }
+
+  const errors = personValidator.validate(data)
+
+  if (errors.length) {
+    response.writeHead(400)
+    response.end(JSON.stringify({ message: errors.join(" ") }))
+
+    return
+  }
+
+  const result = personModel.updateById(personId, data)
+
+  if (!result) {
+    response.writeHead(404)
+    response.end(JSON.stringify({ message: "Person not found" }))
+
+    return
+  }
+
+  response.writeHead(200, { "Content-Type": "application/json" })
+  response.end(JSON.stringify(result))
+})
+
+// Delete person
+router.delete("/person/${personId}", (request, response) => {
+  const { personId } = request.params || {}
+
+  if (!_validateId(personId)) {
+    response.writeHead(400)
+    response.end(JSON.stringify({ message: "Parameter 'personId' is not valid" }))
+
+    return
+  }
+
+  const result = personModel.deleteById(personId)
+
+  if (!result) {
+    response.writeHead(404)
+    response.end(JSON.stringify({ message: "Person not found" }))
+
+    return
+  }
+
+  response.writeHead(204, { "Content-Type": "application/json" })
+  response.end(JSON.stringify(result))
+})
