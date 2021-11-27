e0f9d6929f62818dab4f927194059874a6ece47a
Alexandra Ilyashenko <sasha.ilyashenko@onde.app>
2021-11-27 21:23:35 +0300

    wwerw

--- src/Person/controller.js
+++ src/Person/controller.js
@@ -53,7 +53,7 @@ router.post("/person", (request, response) => {
 
   const result = personModel.create(data)
 
-  response.writeHead(200, { "Content-Type": "application/json" })
+  response.writeHead(201, { "Content-Type": "application/json" })
   response.end(JSON.stringify(result))
 })
 
